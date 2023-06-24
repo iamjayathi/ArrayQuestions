2536e2712b11343f5e35b8cd9ba8f08add531185
iamjayathi <102807749+iamjayathi@users.noreply.github.com>
2023-06-24 22:24:40 +0530

    Add files via upload

---
+++ NLargestElementInArray.java
@@ -0,0 +1,44 @@
+public class NLargestElementInArray {
+    public static void main(String[] args) {
+        int[] arr = {3, 2, 1, 5, 6, 4};
+        int k = 2;
+        System.out.println(findKthLargest(arr, k));
+    }
+
+    static boolean countElement(int mid, int number, int nums[]) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (mid > nums[i]) {
+                count++;
+            }
+        }
+        if (count <= number) return true;
+        else return false;
+    }
+
+    static int findKthLargest(int[] nums, int k) {
+        int n = nums.length;
+        int number = n - k;
+        int start = Integer.MAX_VALUE;
+        int end = Integer.MIN_VALUE;
+        int ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < start) start = nums[i];
+            if (nums[i] > end) end = nums[i];
+        }
+
+        int mid = 0;
+        while (start <= end) {
+            mid = start + ((end - start) / 2);
+
+            if (countElement(mid, number, nums)) {
+                ans = mid;
+                start = mid + 1;
+
+            } else {
+                end = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
